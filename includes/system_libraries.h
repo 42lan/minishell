25ce7d164d24c04c4c84c3e8f30cf420ab5f3669
4slan <amalsago@student.42.fr>
2019-10-09 18:53:48 +0200

    creating header file for to hold included system libraries

---
+++ includes/system_libraries.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   system_libraries.h                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/09 18:49:17 by amalsago          #+#    #+#             */
+/*   Updated: 2019/10/09 18:49:43 by amalsago         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SYSTEM_LIBRARIES_H
+# define SYSTEM_LIBRARIES_H
+
+# include <dirent.h>
+# include <fcntl.h>
+# include <signal.h>
+# include <stdlib.h>
+# include <sys/stat.h>
+# include <sys/wait.h>
+# include <unistd.h>
+
+#endif
