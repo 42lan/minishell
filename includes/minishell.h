b85e18fb68ce4bccd6aca98e2adf8d92fe93002b
4slan <amalsago@student.42.fr>
2019-10-09 18:54:29 +0200

    including system_libraries.h and libft.h as well as prototyping new functions

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,13 +6,17 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/05 12:44:42 by amalsago          #+#    #+#             */
-/*   Updated: 2019/10/05 12:44:49 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/10/09 18:50:11 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include "system_libraries.h"
+# include "../libft/includes/libft.h"
 
+void		display_prompt(char *envp[]);
+void		clear_console(void);
 
 #endif
