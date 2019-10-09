6af281b3e14cabefe5ff760d47fa1fd109a4e59e
4slan <amalsago@student.42.fr>
2019-10-09 18:55:37 +0200

    defining main()

--- sources/main.c
+++ sources/main.c
@@ -6,13 +6,27 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/05 11:45:46 by amalsago          #+#    #+#             */
-/*   Updated: 2019/10/05 11:46:50 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/10/09 18:26:14 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int		main(int argc, char **argv, char ***envp)
+int		main(int argc, char *argv[], char *envp[])
 {
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	char	*command;
+
+	if (argc > 1)
+		command = argv[1];
+	clear_console();
+	while (1)
+	{
+		display_prompt(envp);
+		sleep(10);
+		ft_printf("\n");
+	}
 	return (0);
 }
