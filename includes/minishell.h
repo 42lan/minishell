346303efb936e8d4ca17c94082ce69a7feff19a4
4slan <amalsago@student.42.fr>
2019-10-16 13:26:40 +0200

    including ft_printf.h and get_next_line.h; prototyping get_input()

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/05 12:44:42 by amalsago          #+#    #+#             */
-/*   Updated: 2019/10/09 18:50:11 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/10/16 13:18:39 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,12 @@
 
 # include "system_libraries.h"
 # include "../libft/includes/libft.h"
+# include "../libft/includes/ft_printf.h"
+# include "../libft/includes/get_next_line.h"
+
 
 void		display_prompt(char *envp[]);
 void		clear_console(void);
+char		*get_input(void);
 
 #endif
