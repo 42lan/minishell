fc2970721f9727457c726b147fab285429482ece
4slan <amalsago@student.42.fr>
2019-10-20 17:33:15 +0200

    renaming command to line; calling write_history for each user input

--- sources/main.c
+++ sources/main.c
@@ -6,28 +6,30 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/05 11:45:46 by amalsago          #+#    #+#             */
-/*   Updated: 2019/10/16 13:23:12 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/10/20 17:32:44 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int		main(int argc, char *argv[], char *envp[])
+int				main(int argc, char *argv[], char *envp[])
 {
 	(void)argc;
 	(void)argv;
 	(void)envp;
-	char	*command;
+	char		*line;
 
 	if (argc > 1)
-		command = argv[1];
+		line = argv[1];
 	//clear_console();
 	while (1)
 	{
 		display_prompt(envp);
-		command = get_input();
-		//parse_input(command);
-		ft_printf("%s", command);
+		line = get_input();
+		if (*line)
+			write_history(line);
+		parse_input(line);
+		ft_printf("%s", line);
 		ft_printf("\n");
 	}
 	ft_printf("minishell terminated\n");
