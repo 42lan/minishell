afbbef12fa74b5e240804d39419661e05d32538e
4slan <amalsago@student.42.fr>
2019-10-26 18:41:06 +0200

    opening history file in current directory

--- sources/history.c
+++ sources/history.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/20 16:06:56 by amalsago          #+#    #+#             */
-/*   Updated: 2019/10/20 17:31:38 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/10/21 10:25:34 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	write_history(const char *line)
 
 	mode = S_IRUSR | S_IWUSR;
 	oflag = O_RDWR | O_APPEND | O_CREAT;
-	if ((fd = open("/Users/amalsago/.minishell_history", oflag, mode)) < 0)
+	if ((fd = open(".minishell_history", oflag, mode)) < 0)
 		ft_print_error_and_exit("write_history(): incorrect fd\n");
 	write(fd, line, ft_strlen(line));
 	write(fd, "\n", 1);
