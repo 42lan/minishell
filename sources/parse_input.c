82227fca8703643312b69293e0ad42d0e41620e3
4slan <amalsago@student.42.fr>
2019-10-26 20:34:59 +0200

    splitting line into commands separated by and trimming them

--- sources/parse_input.c
+++ sources/parse_input.c
@@ -6,23 +6,27 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/19 15:24:02 by amalsago          #+#    #+#             */
-/*   Updated: 2019/10/20 16:55:18 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/10/26 20:31:14 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	parse_input(const char *line)
+void		parse_input(const char *line)
 {
+	int		i;
+	char	*tmp;
 	char	**commands;
 
+	i = 0;
 	commands = ft_strsplit(line, ';');
-	while (*commands)
+	while (commands[i])
 	{
-		*commands = ft_strtrim(*commands);
-		ft_printf("->%p<-\n", *commands);
-		commands++;
+		ft_printf("1 [%s]\n", commands[i]);
+		tmp = commands[i];
+		commands[i] = ft_strtrim(commands[i]);
+		ft_strdel(&tmp);
+		ft_printf("2 [%s]\n", commands[i]);
+		++i;
 	}
-	//char	**tokens>
-	//tokens = split_into_tokens(line);
 }
