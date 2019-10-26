aeae595ab8e20ed7980f770f28e6a0b41a2340dd
4slan <amalsago@student.42.fr>
2019-10-26 19:32:41 +0200

    personalising prompt with username

--- sources/display_prompt.c
+++ sources/display_prompt.c
@@ -6,14 +6,19 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/05 14:42:32 by amalsago          #+#    #+#             */
-/*   Updated: 2019/10/07 13:06:10 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/10/26 19:30:24 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	display_prompt(char *envp[])
+void	display_prompt(void)
 {
-	(void)envp;
-	ft_printf("$> ");
+	char	*user;
+
+	user = ft_getenv("USER");
+	if (user)
+		ft_printf("%s$> ", user);
+	else
+		ft_printf("$> ");
 }
