731d89ae0b0bdad409269f00b152c86e5645e172
4slan <amalsago@student.42.fr>
2019-10-27 15:04:41 +0100

    calling new created functions to execute commands and then free memory

--- sources/main.c
+++ sources/main.c
@@ -6,34 +6,36 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/05 11:45:46 by amalsago          #+#    #+#             */
-/*   Updated: 2019/10/26 19:34:32 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/10/27 13:42:09 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-
 int				main(int argc, char *argv[])
 {
-	(void)argc;
-	(void)argv;
-
+	int			i;
 	char		*line;
+	char		**commands;
 
 	if (argc > 1)
 		line = argv[1];
 	//clear_console();
 	while (1)
 	{
+		i = -1;
 		display_prompt();
 		line = get_input();
 		if (*line)
 			write_history(line);
-		parse_input(line);
-		ft_printf("%s", line);
-		ft_printf("\n");
-		ft_strdel(line);
+		commands = parse_input(line);
+		while (commands[++i])
+			execute_command(commands[i]);
+		ft_arraydel(commands);
+		ft_strdel(&line);
 	}
 	ft_printf("minishell terminated\n");
+	(void)argc;
+	(void)argv;
 	return (0);
 }
