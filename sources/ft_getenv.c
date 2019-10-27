dc319a61964178e93e5f675eeba10f8e5c43f76d
4slan <amalsago@student.42.fr>
2019-10-27 15:05:35 +0100

    using interator to loop through environ. otherwise pointer moves

--- sources/ft_getenv.c
+++ sources/ft_getenv.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/26 19:22:45 by amalsago          #+#    #+#             */
-/*   Updated: 2019/10/26 19:22:53 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/10/27 14:19:13 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,21 @@ extern char		**environ;
 
 char			*ft_getenv(const char *name)
 {
+	int			i;
 	char		*key;
 	char		*value;
 
+	i = 0;
 	value = NULL;
-	while (*environ)
+	while (environ[i])
 	{
-		key = ft_strtok(*environ, "=");
+		key = ft_strtok(environ[i], "=");
 		if (ft_strequ(key, name))
 		{
 			value = ft_strtok(NULL, "=");
 			break ;
 		}
-		environ++;
+		i++;
 	}
 	return (value);
 }
