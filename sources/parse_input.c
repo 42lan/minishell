66d8a93723e8e0b42ced0242f6c7ed4149f07981
4slan <amalsago@student.42.fr>
2019-10-27 15:04:06 +0100

    function returns array of char *; removing printfs

--- sources/parse_input.c
+++ sources/parse_input.c
@@ -6,13 +6,13 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/19 15:24:02 by amalsago          #+#    #+#             */
-/*   Updated: 2019/10/26 20:31:14 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/10/27 11:07:09 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void		parse_input(const char *line)
+char		**parse_input(const char *line)
 {
 	int		i;
 	char	*tmp;
@@ -22,11 +22,12 @@ void		parse_input(const char *line)
 	commands = ft_strsplit(line, ';');
 	while (commands[i])
 	{
-		ft_printf("1 [%s]\n", commands[i]);
+		//ft_printf("1 [%s]\n", commands[i]);
 		tmp = commands[i];
 		commands[i] = ft_strtrim(commands[i]);
 		ft_strdel(&tmp);
-		ft_printf("2 [%s]\n", commands[i]);
+		//ft_printf("2 [%s]\n", commands[i]);
 		++i;
 	}
+	return (commands);
 }
