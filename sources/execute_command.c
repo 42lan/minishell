8f1f8fb2fcdd4ec07074857517895da002603a46
4slan <amalsago@student.42.fr>
2019-10-28 18:21:44 +0100

    handeling case if functions returns NULL; checking for exec permission realpath

--- sources/execute_command.c
+++ sources/execute_command.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/27 11:33:04 by amalsago          #+#    #+#             */
-/*   Updated: 2019/10/27 15:58:54 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/10/28 17:15:16 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,29 +17,41 @@ static void		child_handler(const char *command)
 	char		**argv;
 	char		*realpath;
 
-	argv = ft_strsplit(command, ' ');
+	if ((argv = ft_strsplit(command, ' ')) == NULL)
+		ft_perror_exit("ft_strsplit() failed in child_handler()");
 	realpath = find_executable(argv[0]);
+	if (access(realpath, X_OK) != 0)
+	{
+		ft_perror("minishell: permission denied: ");
+		ft_perror_exit(realpath);
+	}
 	if ((execve(realpath, argv, NULL)) < 0)
-		ft_print_error_and_exit("execute_command(): execve() failed\n");
+		ft_perror_exit("execute_command(): execve() failed");
 }
 
 int			execute_command(const char *command)
 {
+	int		stat_loc;
+	(void)stat_loc;
 	pid_t	pid;
 
 	if (!command || !*command)
 		return (-1);
-	pid = fork();
-	if (pid < 0)
-		ft_print_error_and_exit("execute_command(): fork() failed\n");
+	if ((pid = fork()) < 0)
+		ft_perror_exit("fork() failed in execute_command.c");
 	else if (pid == 0)
 	{
+		//ft_printf("Child execution\n");
 		/* CHILD EXECUTES COMMAND */
 		child_handler(command);
+		// Need to exit
 	}
 	else
 	{
+		//ft_printf("Parent execution\n");
 		/* PARENT WAITS WHILE HIS CHILD EXECVE COMMAND */
+		// parent_handler();
+		wait(NULL);
 	}
 	return (1);
 }
