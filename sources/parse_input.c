78ff2ebce2370ff3ccd353649228cf22c9080af2
4slan <amalsago@student.42.fr>
2019-10-28 12:06:37 +0100

    handeling NULL return of functions and calling printing error function by its new name

--- sources/parse_input.c
+++ sources/parse_input.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/19 15:24:02 by amalsago          #+#    #+#             */
-/*   Updated: 2019/10/27 11:07:09 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/10/28 12:05:38 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,14 @@ char		**parse_input(const char *line)
 	char	**commands;
 
 	i = 0;
-	commands = ft_strsplit(line, ';');
+	if ((commands = ft_strsplit(line, ';')) == NULL)
+		ft_perror_exit("ft_strsplit() failed in parse_input.c");
 	while (commands[i])
 	{
-		//ft_printf("1 [%s]\n", commands[i]);
 		tmp = commands[i];
-		commands[i] = ft_strtrim(commands[i]);
+		if ((commands[i] = ft_strtrim(commands[i])) == NULL)
+			ft_perror_exit("ft_strtrim() failed in parse_input.c");
 		ft_strdel(&tmp);
-		//ft_printf("2 [%s]\n", commands[i]);
 		++i;
 	}
 	return (commands);
