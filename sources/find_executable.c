3093faa70f0b5a0439b71f3353ddbe895696a7ae
4slan <amalsago@student.42.fr>
2019-10-28 12:09:42 +0100

    handeling case if executable is given as relative or absolute path

--- sources/find_executable.c
+++ sources/find_executable.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/27 15:55:36 by amalsago          #+#    #+#             */
-/*   Updated: 2019/10/27 15:56:19 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/10/28 10:52:20 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,19 @@ char		*find_executable(const char *executable)
 	char	*directories;
 
 	realpath = NULL;
-	directories = ft_getenv("PATH");
-	directory = ft_strtok(directories, ":");
-	while (directory)
+	if (executable[0] == '/' || ft_strncmp(executable, "./", 2) == 0)
+		realpath = ft_strdup(executable);
+	else
 	{
-		directory = ft_strtok(NULL, ":");
-		realpath = ft_realpath(directory, executable);
-		if (access(realpath, F_OK) == 0)
-			break ;
+		directories = ft_getenv("PATH");
+		directory = ft_strtok(directories, ":");
+		while (directory)
+		{
+			directory = ft_strtok(NULL, ":");
+			realpath = ft_realpath(directory, executable);
+			if (access(realpath, F_OK) == 0)
+				break ;
+		}
 	}
 	return (realpath);
 }
