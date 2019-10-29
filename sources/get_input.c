1442bc29c4e43957988b73c7ffff7617cecb6fed
4slan <amalsago@student.42.fr>
2019-10-29 19:54:51 +0100

    freeing memory before quit program

--- sources/get_input.c
+++ sources/get_input.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/15 13:29:03 by amalsago          #+#    #+#             */
-/*   Updated: 2019/10/28 12:00:00 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/10/29 19:54:35 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,16 @@ char		*get_input(void)
 	char	*input;
 
 	if (get_next_line(0, &input) <= 0)
+	{
+		ft_strdel(&input);
 		ft_perror_exit("GNL call returned value < 1 in get_input()");
+	}
 	tmp = input;
 	if ((input = ft_strtrim(input)) == NULL)
+	{
+		ft_strdel(&input);
 		ft_perror_exit("ft_strtrim() call failed in get_input()");
+	}
 	ft_strdel(&tmp);
 	return (input);
 }
