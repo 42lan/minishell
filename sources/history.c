816257a3171969cbe765d3be62f179330b855b2c
4slan <amalsago@student.42.fr>
2019-11-02 19:12:02 +0100

    englobing write and close calls in else statement

--- sources/history.c
+++ sources/history.c
@@ -6,13 +6,13 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/20 16:06:56 by amalsago          #+#    #+#             */
-/*   Updated: 2019/10/28 12:02:34 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/11/02 19:02:23 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	write_history(const char *line)
+void		write_history(const char *line)
 {
 	int		fd;
 	int		mode;
@@ -21,8 +21,11 @@ void	write_history(const char *line)
 	mode = S_IRUSR | S_IWUSR;
 	oflag = O_RDWR | O_APPEND | O_CREAT;
 	if ((fd = open(".minishell_history", oflag, mode)) < 0)
-		ft_perror_exit("open() failed in write_history.c");
-	write(fd, line, ft_strlen(line));
-	write(fd, "\n", 1);
-	close(fd);
+		ft_perror("minishell: open() failed in write_history()");
+	else
+	{
+		write(fd, line, ft_strlen(line));
+		write(fd, "\n", 1);
+		close(fd);
+	}
 }
