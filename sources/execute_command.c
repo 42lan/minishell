18b55d3033504db25b2c56c907f55b2383aff948
4slan <amalsago@student.42.fr>
2019-11-02 19:08:51 +0100

    allocating memory in parent process by calling strsplit and find_executable to be freed at the end

--- sources/execute_command.c
+++ sources/execute_command.c
@@ -6,50 +6,45 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/27 11:33:04 by amalsago          #+#    #+#             */
-/*   Updated: 2019/10/29 20:01:35 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/11/02 19:06:54 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void		child_handler(const char *command)
+extern char		**environ;
+
+void			child_handler(const char *realpath, char *const argv[])
 {
+	if (check_access(realpath))
+		if ((execve(realpath, argv, environ)) < 0)
+			ft_perror("minishell: execve() failed in child_handler()");
+	exit(EXIT_FAILURE);
+}
+
+int				execute_command(const char *command)
+{
+	pid_t		pid;
 	char		**argv;
 	char		*realpath;
 
-	if ((argv = ft_strsplit(command, ' ')) == NULL)
-		ft_perror_exit("ft_strsplit() failed in child_handler()");
-	realpath = find_executable(argv[0]);
-	if (check_access(realpath))
-		if ((execve(realpath, argv, NULL)) < 0)
-			ft_perror_exit("execute_command(): execve() failed");
-	ft_arraydel(argv);
-	ft_strdel(&realpath);
-}
-
-int			execute_command(const char *command)
-{
-	int		stat_loc;
-	(void)stat_loc;
-	pid_t	pid;
-
 	if (!command || !*command)
 		return (-1);
+	if ((argv = ft_strsplit(command, ' ')) == NULL)
+	{
+		ft_perror("minishell: ft_strsplit() failed in execute_command()");
+		return (-1);
+	}
+	realpath = find_executable(argv[0]);
 	if ((pid = fork()) < 0)
-		ft_perror_exit("fork() failed in execute_command.c");
+	{
+		ft_perror("minishell: fork() failed in execute_command.c");
+		exit(EXIT_FAILURE);
+	}
 	else if (pid == 0)
-	{
-		//ft_printf("Child execution\n");
-		/* CHILD EXECUTES COMMAND */
-		child_handler(command);
-		// Need to exit
-	}
-	else
-	{
-		//ft_printf("Parent execution\n");
-		/* PARENT WAITS WHILE HIS CHILD EXECVE COMMAND */
-		// parent_handler();
-		wait(NULL);
-	}
+		child_handler(realpath, argv);
+	waitpid(pid, NULL, 0);
+	ft_arraydel(argv);
+	ft_strdel(&realpath);
 	return (1);
 }
