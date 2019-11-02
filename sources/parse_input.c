b13558d4a8c46d1d6be17012f69ef0abe300476f
4slan <amalsago@student.42.fr>
2019-11-02 19:12:48 +0100

    adding else statement where strtrim is called

--- sources/parse_input.c
+++ sources/parse_input.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/19 15:24:02 by amalsago          #+#    #+#             */
-/*   Updated: 2019/10/28 12:05:38 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/11/02 19:02:57 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,17 @@ char		**parse_input(const char *line)
 	char	**commands;
 
 	i = 0;
+	commands = NULL;
 	if ((commands = ft_strsplit(line, ';')) == NULL)
-		ft_perror_exit("ft_strsplit() failed in parse_input.c");
-	while (commands[i])
-	{
-		tmp = commands[i];
-		if ((commands[i] = ft_strtrim(commands[i])) == NULL)
-			ft_perror_exit("ft_strtrim() failed in parse_input.c");
-		ft_strdel(&tmp);
-		++i;
-	}
+		ft_perror("minishell: ft_strsplit() failed in parse_input()");
+	else
+		while (commands[i])
+		{
+			tmp = commands[i];
+			if ((commands[i] = ft_strtrim(commands[i])) == NULL)
+				ft_perror("minishell: ft_strtrim() failed in parse_input()");
+			ft_strdel(&tmp);
+			++i;
+		}
 	return (commands);
 }
