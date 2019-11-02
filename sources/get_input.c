130dab2d0a881cae26e6e078516380df98624d1f
4slan <amalsago@student.42.fr>
2019-11-02 19:10:42 +0100

    adding condition before triming input

--- sources/get_input.c
+++ sources/get_input.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/15 13:29:03 by amalsago          #+#    #+#             */
-/*   Updated: 2019/10/29 19:54:35 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/11/02 19:01:55 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,17 @@ char		*get_input(void)
 	if (get_next_line(0, &input) <= 0)
 	{
 		ft_strdel(&input);
-		ft_perror_exit("GNL call returned value < 1 in get_input()");
+		ft_perror("minishell: GNL call returned value < 1 in get_input()");
 	}
-	tmp = input;
-	if ((input = ft_strtrim(input)) == NULL)
+	if (*input)
 	{
-		ft_strdel(&input);
-		ft_perror_exit("ft_strtrim() call failed in get_input()");
+		tmp = input;
+		if ((input = ft_strtrim(input)) == NULL)
+		{
+			ft_strdel(&input);
+			ft_perror("minishell: ft_strtrim() failed in get_input()");
+		}
+		ft_strdel(&tmp);
 	}
-	ft_strdel(&tmp);
 	return (input);
 }
