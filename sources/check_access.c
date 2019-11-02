57ef1e1379446ab1f5c4e8b65471ac32faaab79c
4slan <amalsago@student.42.fr>
2019-11-02 19:07:55 +0100

    joing error and executable name to be printed

--- sources/check_access.c
+++ sources/check_access.c
@@ -6,24 +6,28 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/29 17:25:08 by amalsago          #+#    #+#             */
-/*   Updated: 2019/10/29 19:38:30 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/11/02 19:06:51 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int		check_access(const char *realpath)
+int			check_access(const char *realpath)
 {
+	char	*error;
+
 	if (access(realpath, F_OK) != 0)
 	{
-		ft_perror("minishell: no such file or directory: ");
-		ft_perror(realpath);
+		error = ft_strjoin("minishell: no such file or directory: ", realpath);
+		ft_perror(error);
+		ft_strdel(&error);
 		return (0);
 	}
 	if (access(realpath, X_OK) != 0)
 	{
-		ft_perror("minishell: permission denied: ");
-		ft_perror(realpath);
+		error = ft_strjoin("minishell: permission denied: ", realpath);
+		ft_perror(error);
+		ft_strdel(&error);
 		return (0);
 	}
 	return (1);
