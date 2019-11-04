7c2580536841e12d2f605e4cb352db93bb3bcac5
4slan <amalsago@student.42.fr>
2019-11-04 16:46:51 +0100

    refactoring: creationg helper function

--- sources/find_executable.c
+++ sources/find_executable.c
@@ -6,36 +6,41 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/27 15:55:36 by amalsago          #+#    #+#             */
-/*   Updated: 2019/10/29 17:24:16 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/11/04 10:55:44 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char		*find_executable(const char *executable)
+static char		*get_realpath(const char *executable)
 {
-	char	*realpath;
-	char	*directory;
-	char	*directories;
+	char		*realpath;
+	char		*directory;
+	char		*directories;
+
+	directories = ft_strdup(ft_getenv("PATH"));
+	directory = ft_strtok(directories, ":");
+	while (directory)
+	{
+		realpath = ft_realpath(directory, executable);
+		if (access(realpath, F_OK) == 0)
+			break ;
+		directory = ft_strtok(NULL, ":");
+		ft_strdel(&realpath);
+	}
+	ft_strdel(&directories);
+	return (realpath);
+}
+
+char			*find_executable(const char *executable)
+{
+	char		*realpath;
 
 	realpath = NULL;
 	if (ft_strnequ(executable, "/", 1) || ft_strnequ(executable, "./", 2))
-		realpath = ft_strdup(executable);
+		realpath = (char *)executable;
 	else
-	{
-		directories = ft_strdup(ft_getenv("PATH"));
-		directory = ft_strtok(directories, ":");
-		while (directory)
-		{
-			realpath = ft_realpath(directory, executable);
-			if (access(realpath, F_OK) == 0)
-				break ;
-			directory = ft_strtok(NULL, ":");
-			ft_strdel(&realpath);
-		}
-		if (!realpath)
-			realpath = ft_strdup(executable);
-	}
-	ft_strdel(&directories);
+		if (ft_getenv("PATH"))
+			realpath = get_realpath(executable);
 	return (realpath);
 }
