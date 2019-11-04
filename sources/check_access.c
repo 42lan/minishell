44ce90a6b57a5c9ef627b453baa6970867691bdb
4slan <amalsago@student.42.fr>
2019-11-04 11:22:09 +0100

    calling corresponding functions

--- sources/check_access.c
+++ sources/check_access.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/29 17:25:08 by amalsago          #+#    #+#             */
-/*   Updated: 2019/11/02 19:06:51 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/11/04 10:49:05 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,14 @@
 
 int			check_access(const char *realpath)
 {
-	char	*error;
-
 	if (access(realpath, F_OK) != 0)
 	{
-		error = ft_strjoin("minishell: no such file or directory: ", realpath);
-		ft_perror(error);
-		ft_strdel(&error);
+		print_enoent(realpath);
 		return (0);
 	}
 	if (access(realpath, X_OK) != 0)
 	{
-		error = ft_strjoin("minishell: permission denied: ", realpath);
-		ft_perror(error);
-		ft_strdel(&error);
+		print_eacces(realpath);
 		return (0);
 	}
 	return (1);
