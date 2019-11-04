6f7a185e7e9268d57e68c240c74064cabc4b1f4a
4slan <amalsago@student.42.fr>
2019-11-04 16:45:53 +0100

    refactoring: calling error printing functions

--- sources/execute_command.c
+++ sources/execute_command.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/27 11:33:04 by amalsago          #+#    #+#             */
-/*   Updated: 2019/11/02 19:06:54 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/11/04 16:44:34 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 extern char		**environ;
 
-void			child_handler(const char *realpath, char *const argv[])
+void			child_handler(const char *realpath, char *const *argv)
 {
 	if (check_access(realpath))
 		if ((execve(realpath, argv, environ)) < 0)
@@ -22,29 +22,26 @@ void			child_handler(const char *realpath, char *const argv[])
 	exit(EXIT_FAILURE);
 }
 
-int				execute_command(const char *command)
+int				execute_command(char **command)
 {
 	pid_t		pid;
-	char		**argv;
 	char		*realpath;
 
-	if (!command || !*command)
-		return (-1);
-	if ((argv = ft_strsplit(command, ' ')) == NULL)
+	if (!(realpath = find_executable(command[0])))
 	{
-		ft_perror("minishell: ft_strsplit() failed in execute_command()");
+		print_enofound(command[0]);
+		ft_arraydel(command);
 		return (-1);
 	}
-	realpath = find_executable(argv[0]);
 	if ((pid = fork()) < 0)
 	{
-		ft_perror("minishell: fork() failed in execute_command.c");
+		ft_perror("minishell: fork() failed in execute_command()");
+		ft_arraydel(command);
 		exit(EXIT_FAILURE);
 	}
 	else if (pid == 0)
-		child_handler(realpath, argv);
+		child_handler(realpath, command);
 	waitpid(pid, NULL, 0);
-	ft_arraydel(argv);
-	ft_strdel(&realpath);
+	ft_arraydel(command);
 	return (1);
 }
