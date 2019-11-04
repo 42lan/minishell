099caf839d337346cc9eefffe2a239199b4538b5
4slan <amalsago@student.42.fr>
2019-11-04 19:03:06 +0100

    addign new condition to call other functions

--- sources/execute_builtin.c
+++ sources/execute_builtin.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 14:31:37 by amalsago          #+#    #+#             */
-/*   Updated: 2019/11/04 16:05:53 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/11/04 18:32:22 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,18 @@
 
 void		execute_builtin(const char *command, const char *line)
 {
-	if (ft_strequ(command, "exit"))
-		builtin_exit();
-	else if (ft_strequ(command, "echo"))
+	/*
+	if (ft_strequ(command, "cd"))
+		builtin_cd();
+	*/
+	if (ft_strequ(command, "echo"))
 		builtin_echo(line);
 	else if (ft_strequ(command, "env"))
 		builtin_env();
+	else if (ft_strequ(command, "exit"))
+		builtin_exit();
+	else if (ft_strequ(command, "setenv"))
+		builtin_setenv(line);
 /*
 	else if (ft_strequ(command, "cd"))
 		builtin_cd();
