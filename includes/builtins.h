a4db010035112c9d05ddc8b8e9c03d946b2a62cd
4slan <amalsago@student.42.fr>
2019-11-04 16:27:58 +0100

    placing predicatior is_builtin() in minishell.h; prototyping builtin_echo()

--- includes/builtins.h
+++ includes/builtins.h
@@ -6,14 +6,14 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 12:10:52 by amalsago          #+#    #+#             */
-/*   Updated: 2019/11/04 14:05:02 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/11/04 16:26:33 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUILTINS_H
 # define BUILTINS_H
 
-int		is_builtin(const char *command);
+void	builtin_echo(const char *line);
 void	builtin_env(void);
 void	builtin_exit(void);
 
