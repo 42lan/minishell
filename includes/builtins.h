fd29b6bf4c0f314dc9bb8987b9b0c3e14bac3e7a
4slan <amalsago@student.42.fr>
2019-11-04 18:58:24 +0100

    prototyping builtin_setenv()

--- includes/builtins.h
+++ includes/builtins.h
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 12:10:52 by amalsago          #+#    #+#             */
-/*   Updated: 2019/11/04 16:26:33 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/11/04 18:37:58 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 void	builtin_echo(const char *line);
 void	builtin_env(void);
 void	builtin_exit(void);
+void	builtin_setenv(const char *line);
 
 static char *g_builtins[] = {
 	"cd",
