942e3e50f44b531440575b439273bf92d8d5bb6b
4slan <amalsago@student.42.fr>
2019-11-04 19:23:28 +0100

    removing child_handler() and placing its instruction in execute_command()

--- sources/execute_command.c
+++ sources/execute_command.c
@@ -6,21 +6,13 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/27 11:33:04 by amalsago          #+#    #+#             */
-/*   Updated: 2019/11/04 16:52:02 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/11/04 19:14:36 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 extern char		**environ;
-
-void			child_handler(const char *realpath, char *const *argv)
-{
-	if (check_access(realpath))
-		if ((execve(realpath, argv, environ)) < 0)
-			ft_perror("minishell: execve() failed in child_handler()");
-	exit(EXIT_FAILURE);
-}
 
 int				execute_command(char **command)
 {
@@ -32,7 +24,10 @@ int				execute_command(char **command)
 		ft_arraydel(command);
 		return (-1);
 	}
-	child_handler(realpath, command);
+	if (check_access(realpath))
+		if ((execve(realpath, command, environ)) < 0)
+			ft_perror("minishell: execve() failed in child_handler()");
+	exit(EXIT_FAILURE);
 	ft_strdel(&realpath);
 	return (1);
 }
