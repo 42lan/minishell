f0d040e10f66823af820b4d3a52223ee83379025
4slan <amalsago@student.42.fr>
2019-11-06 10:51:31 +0100

    removing ft_strtok() because it modifies environ variable; retrieving value by searching '=' char + 1

--- libft/sources/string/ft_getenv.c
+++ libft/sources/string/ft_getenv.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/26 19:22:45 by amalsago          #+#    #+#             */
-/*   Updated: 2019/11/04 19:25:58 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/11/06 10:23:50 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,15 @@ extern char		**environ;
 char			*ft_getenv(const char *name)
 {
 	int			i;
-	char		*key;
 	char		*value;
 
 	i = 0;
 	value = NULL;
 	while (environ[i])
 	{
-		key = ft_strtok(environ[i], "=");
-		if (ft_strequ(key, name))
+		if (ft_strnequ(environ[i], name, ft_strlen(name)))
 		{
-			value = ft_strtok(NULL, "=");
+			value = ft_strchr(environ[i], '=') + 1;
 			break ;
 		}
 		i++;
