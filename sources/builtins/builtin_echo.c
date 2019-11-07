075979278ddcf0a90657827b82e3c9eda8796349
4slan <amalsago@student.42.fr>
2019-11-07 16:28:36 +0100

    adding call to exit to terminate child process

--- sources/builtins/builtin_echo.c
+++ sources/builtins/builtin_echo.c
@@ -6,17 +6,17 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 14:50:41 by amalsago          #+#    #+#             */
-/*   Updated: 2019/11/04 19:01:22 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/11/06 11:13:34 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void			builtin_echo(const char *line)
+void		builtin_echo(const char *line)
 {
-	char		*args;
-	char		*token;
-	int			last_char;
+	char	*args;
+	char	*token;
+	int		last_char;
 
 	args = ft_strtrim(line + 4);
 	if (args[0] == '"')
@@ -37,4 +37,5 @@ void			builtin_echo(const char *line)
 		}
 	}
 	ft_strdel(&args);
+	exit(EXIT_SUCCESS);
 }
