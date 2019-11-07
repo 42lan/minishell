5a91fdd6b9809c1a94263eb7838fd6f67739e54c
4slan <amalsago@student.42.fr>
2019-11-07 16:28:57 +0100

    adding call to exit to terminate child process

--- sources/builtins/builtin_env.c
+++ sources/builtins/builtin_env.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 14:02:04 by amalsago          #+#    #+#             */
-/*   Updated: 2019/11/04 14:07:24 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/11/06 11:40:19 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,9 @@ void			builtin_env(void)
 	int			i;
 
 	i = -1;
+	if (!environ || !*environ)
+		exit(EXIT_FAILURE);
 	while (environ[++i])
 		ft_printf("%s\n", environ[i]);
+	exit(EXIT_SUCCESS);
 }
