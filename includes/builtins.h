ba19b33c1eae5882293260c5215bbada803cbab4
4slan <amalsago@student.42.fr>
2019-11-07 19:01:11 +0100

    prototyping builtin_cd()

--- includes/builtins.h
+++ includes/builtins.h
@@ -6,13 +6,14 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 12:10:52 by amalsago          #+#    #+#             */
-/*   Updated: 2019/11/04 18:37:58 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/11/07 18:59:37 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUILTINS_H
 # define BUILTINS_H
 
+void	builtin_cd(const char *line);
 void	builtin_echo(const char *line);
 void	builtin_env(void);
 void	builtin_exit(void);
