65d9ef1cb50f2c29e34e14d5517f6ec5a8f22c37
4slan <amalsago@student.42.fr>
2019-11-08 21:12:41 +0100

    using return statements and adding manual comment

--- sources/builtins/builtin_env.c
+++ sources/builtins/builtin_env.c
@@ -6,22 +6,30 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 14:02:04 by amalsago          #+#    #+#             */
-/*   Updated: 2019/11/06 11:40:19 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/11/08 21:02:43 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+/*
+** DESCRIPTION
+**	builtin_env()
+**
+** RETURN VALUES
+**	Upon successful completion, the value 1 is returned otherwise the value 0.
+*/
+
 extern char		**environ;
 
-void			builtin_env(void)
+int				builtin_env(void)
 {
 	int			i;
 
 	i = -1;
 	if (!environ || !*environ)
-		exit(EXIT_FAILURE);
+		return (0);
 	while (environ[++i])
 		ft_printf("%s\n", environ[i]);
-	exit(EXIT_SUCCESS);
+	return (1);
 }
