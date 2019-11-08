d097b2372a412d16b634f60b1773782549bf083f
4slan <amalsago@student.42.fr>
2019-11-08 21:16:55 +0100

    executing builtins in parent process; adding manual comment

--- sources/execute.c
+++ sources/execute.c
@@ -6,19 +6,19 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 12:28:51 by amalsago          #+#    #+#             */
-/*   Updated: 2019/11/06 11:10:53 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/11/08 20:52:53 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void		child_handler(const char *line, char **argv)
-{
-	if (is_builtin(argv[0]))
-		execute_builtin(argv[0], line);
-	else
-		execute_command(argv);
-}
+/*
+** DESCRIPTION
+**	execute()
+**
+** RETURN VALUES
+**	Upon successful completion, the value 1 is returned otherwise the value 0.
+*/
 
 int			execute(const char *line)
 {
@@ -26,25 +26,25 @@ int			execute(const char *line)
 	char	**argv;
 
 	if (!line || !*line)
-		return (-1);
+		return (0);
 	if ((argv = ft_strsplit(line, ' ')) == NULL)
 	{
 		ft_perror("minishell: ft_strsplit() failed in execute_command()");
-		return (-1);
+		return (0);
 	}
-	if (ft_strequ(argv[0], "exit"))
+	if (is_builtin(argv[0]))
+		execute_builtin(argv[0], line);
+	else
 	{
-		ft_arraydel(argv);
-		builtin_exit();
+		if ((pid = fork()) < 0)
+		{
+			ft_perror("minishell: fork() failed in execute_command()");
+			return (0);
+		}
+		else if (pid == 0)
+			execute_command(argv);
+		waitpid(pid, NULL, 0);
 	}
-	if ((pid = fork()) < 0)
-	{
-		ft_perror("minishell: fork() failed in execute_command()");
-		return (-1);
-	}
-	else if (pid == 0)
-		child_handler(line, argv);
-	waitpid(pid, NULL, 0);
 	ft_arraydel(argv);
 	return (1);
 }
