2833cbef7d0a155177b875ed016652804283d92b
4slan <amalsago@student.42.fr>
2019-11-08 21:10:28 +0100

    adding return type int for builtins except builtin_exit()

--- includes/builtins.h
+++ includes/builtins.h
@@ -6,18 +6,19 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 12:10:52 by amalsago          #+#    #+#             */
-/*   Updated: 2019/11/07 18:59:37 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/11/08 19:05:36 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUILTINS_H
 # define BUILTINS_H
 
-void	builtin_cd(const char *line);
-void	builtin_echo(const char *line);
-void	builtin_env(void);
-void	builtin_exit(void);
-void	builtin_setenv(const char *line);
+int			builtin_cd(const char *line);
+int			builtin_echo(const char *line);
+int			builtin_env(void);
+void		builtin_exit(void);
+int			builtin_setenv(const char *line);
+//void	builtin_which(const char *line);
 
 static char *g_builtins[] = {
 	"cd",
