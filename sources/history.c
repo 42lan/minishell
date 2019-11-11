321e3f7fe09420d569bc399a2f99abfdef7fc6fd
4slan <amalsago@student.42.fr>
2019-11-11 14:38:32 +0100

    adding new static variable to hold formed path for history file

--- sources/history.c
+++ sources/history.c
@@ -6,21 +6,24 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/20 16:06:56 by amalsago          #+#    #+#             */
-/*   Updated: 2019/11/02 19:02:23 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/11/11 14:32:19 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void		write_history(const char *line)
+void				write_history(const char *line)
 {
-	int		fd;
-	int		mode;
-	int		oflag;
+	int				fd;
+	int				mode;
+	int				oflag;
+	static char		*history_file;
 
 	mode = S_IRUSR | S_IWUSR;
 	oflag = O_RDWR | O_APPEND | O_CREAT;
-	if ((fd = open(".minishell_history", oflag, mode)) < 0)
+	if (!history_file)
+		history_file = form_path(ft_getenv("HOME"), ".minishell_history");
+	if ((fd = open(history_file, oflag, mode)) < 0)
 		ft_perror("minishell: open() failed in write_history()");
 	else
 	{
