d76fd8602509ab674e44de9b5ee4c2b1b0d02322
4slan <amalsago@student.42.fr>
2019-11-11 14:34:51 +0100

    removing red coloration of prompt

--- sources/display_prompt.c
+++ sources/display_prompt.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/05 14:42:32 by amalsago          #+#    #+#             */
-/*   Updated: 2019/11/11 12:23:22 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/11/11 14:24:58 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,8 @@ void				display_prompt(void)
 
 	if (!user)
 		user = ft_getenv("USER");
-	ft_putstr(SGR_FG_RED);
 	if (user)
 		ft_printf("%s$> ", user);
 	else
 		ft_printf("$> ");
-	ft_putstr(SGR_NORMAL);
 }
