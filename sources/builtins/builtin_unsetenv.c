fcf03571e2f59f5f2e3206950dd7a4c954542d3e
4slan <amalsago@student.42.fr>
2019-11-12 18:31:30 +0100

    removing definition of builtin_unsetenv() and refactoring ft_unsetenv() by trimming input

--- sources/builtins/builtin_unsetenv.c
+++ sources/builtins/builtin_unsetenv.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/10 12:07:18 by amalsago          #+#    #+#             */
-/*   Updated: 2019/11/11 13:46:28 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/11/11 16:48:15 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,14 @@
 
 /*
 ** DESCRIPTION
-**	builtin_unsetenv()
+**	ft_unsetenv() removes an environment variable from the environment of the
+**	calling process. The `name` argument points to a string, which contains
+**  the name of the variable to be removed. Before processing, function performs
+**	trimming of `name` to remove whitespaces at the beginning and the end of the
+**	string.
 **
 ** RETURN VALUES
-**	Upon successful completion, the value 1 is returned otherwise
+**	Upon successful completion, the value 0 is returned otherwise the value 1.
 */
 
 extern char		**environ;
@@ -25,26 +29,20 @@ extern char		**environ;
 int				ft_unsetenv(const char *name)
 {
 	int			i;
+	char		*trimmed_name;
 
 	i = -1;
-	if (!name || !*name)
+	trimmed_name = ft_strtrim(name);
+	if (!trimmed_name || !*trimmed_name)
 	{
 		ft_perror("unsetenv: Too few arguments.");
-		return (0);
+		ft_strdel(&trimmed_name);
+		return (1);
 	}
 	while (environ[++i])
-		if (ft_strnequ(environ[i], name, ft_strlen(name)))
+	if (ft_strnequ(environ[i], trimmed_name, ft_strlen(trimmed_name)))
 			break ;
 	ft_strdel(&environ[i]);
-	return (1);
-}
-
-int				builtin_unsetenv(const char *line)
-{
-	char		*name;
-
-	name = ft_strtrim(line + 8);
-	ft_unsetenv(name);
-	ft_strdel(&name);
-	return (1);
+	ft_strdel(&trimmed_name);
+	return (0);
 }
