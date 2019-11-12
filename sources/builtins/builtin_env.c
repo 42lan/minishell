c84b06943ea0aec9af2f78b1ed6c0008e296781a
4slan <amalsago@student.42.fr>
2019-11-12 18:32:26 +0100

    modifying return values

--- sources/builtins/builtin_env.c
+++ sources/builtins/builtin_env.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 14:02:04 by amalsago          #+#    #+#             */
-/*   Updated: 2019/11/08 21:02:43 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/11/11 18:38:31 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 **	builtin_env()
 **
 ** RETURN VALUES
-**	Upon successful completion, the value 1 is returned otherwise the value 0.
+**	Upon successful completion, the value 0 is returned.
 */
 
 extern char		**environ;
@@ -28,8 +28,8 @@ int				builtin_env(void)
 
 	i = -1;
 	if (!environ || !*environ)
-		return (0);
+		return (1);
 	while (environ[++i])
 		ft_printf("%s\n", environ[i]);
-	return (1);
+	return (0);
 }
