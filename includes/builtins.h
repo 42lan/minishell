a39a19160a31fae9976631f24e9ba8ffc5384a0e
4slan <amalsago@student.42.fr>
2019-11-12 18:33:16 +0100

    prototyping ft_setenv() and ft_unsetenv(); removing builtin_unsetenv()

--- includes/builtins.h
+++ includes/builtins.h
@@ -6,12 +6,15 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 12:10:52 by amalsago          #+#    #+#             */
-/*   Updated: 2019/11/11 15:44:42 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/11/12 18:32:59 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUILTINS_H
 # define BUILTINS_H
+
+int				ft_setenv(const char *name, const char *value);
+int				ft_unsetenv(const char *name);
 
 int				builtin_builtins(const char *args);
 int				builtin_cd(const char *line);
@@ -19,7 +22,6 @@ int				builtin_echo(const char *line);
 int				builtin_env(void);
 void			builtin_exit(void);
 int				builtin_setenv(const char *line);
-int				builtin_unsetenv(const char *line);
 
 static char		*g_builtins[] = {
 	"builtins",
