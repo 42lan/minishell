4c756c4b41a9fb6ece569ee464ba74d2b792526b
4slan <amalsago@student.42.fr>
2019-11-13 15:53:27 +0100

    Print list of builtins in column
    To print list of builtins in column new functions were defined:
        get_max_len() computate maximal name length of builtins;
        get_per_row() computate number of name to be in one row;
    check_many_arguments() performing check if there is no arguments after
    the command.

--- sources/builtins/builtin_builtins.c
+++ sources/builtins/builtin_builtins.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/11 14:40:55 by amalsago          #+#    #+#             */
-/*   Updated: 2019/11/11 15:47:09 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/11/13 12:49:43 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,21 +21,66 @@
 **	Upon successful completion, the value 0 is returned otherwise the value 1.
 */
 
-int			builtin_builtins(const char *args)
+static size_t		get_max_len(void)
 {
-	int		i;
+	size_t			i;
+	size_t			len;
+	size_t			max_len;
 
 	i = -1;
-	while (ft_iswhitespace(*args))
+	max_len = 0;
+	while (g_builtins[++i])
 	{
-		if (*args)
-		{
-			ft_perror("builtins: Too many arguments.");
+		len = ft_strlen(g_builtins[i]);
+		if (len > max_len)
+			max_len = len;
+	}
+	return (max_len);
+}
+
+static size_t		get_per_row(size_t max_name_len)
+{
+	size_t			per_row;
+	struct winsize	terminal;
+
+	ioctl(STDIN_FILENO, TIOCGWINSZ, &terminal);
+	per_row = (terminal.ws_col / max_name_len) - 1;
+	return ((per_row < 1) ? 1 : per_row);
+}
+
+static int			check_many_arguments(const char *args)
+{
+	int				i;
+
+	i = -1;
+	while (ft_iswhitespace(args[++i]))
+		if (args[i])
 			return (1);
-		}
-		args++;
+	return (0);
+}
+
+int					builtin_builtins(const char *args)
+{
+	size_t			i;
+	size_t			width;
+	size_t			per_row;
+	size_t			max_name_len;
+
+	i = -1;
+	max_name_len = get_max_len();
+	per_row = get_per_row(max_name_len);
+	if (check_many_arguments(args))
+	{
+		ft_perror("builtins: Too many arguments.");
+		return (1);
 	}
 	while (g_builtins[++i])
-		ft_printf("%s\n", g_builtins[i]);
+	{
+		if (i == per_row - 1)
+			ft_putchar('\n');
+		width = max_name_len + 5;
+		ft_printf("%-*s", width, g_builtins[i]);
+	}
+	ft_putchar('\n');
 	return (0);
 }
