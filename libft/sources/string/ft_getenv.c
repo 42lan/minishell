c09d519a88402fcf97b37755184ba81409853e7d
4slan <amalsago@student.42.fr>
2019-11-14 11:58:46 +0100

    Return value of exact environ variable
    This fix will return value of exact environ variable because condition
    performs check for equal sign '=' at `len` position of environ variable.

--- libft/sources/string/ft_getenv.c
+++ libft/sources/string/ft_getenv.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/26 19:22:45 by amalsago          #+#    #+#             */
-/*   Updated: 2019/11/06 10:23:50 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/11/14 11:57:52 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,15 @@ extern char		**environ;
 char			*ft_getenv(const char *name)
 {
 	int			i;
+	int			len;
 	char		*value;
 
 	i = 0;
 	value = NULL;
 	while (environ[i])
 	{
-		if (ft_strnequ(environ[i], name, ft_strlen(name)))
+		len = ft_strlen(name);
+		if (ft_strnequ(environ[i], name, len) && environ[i][len] == '=')
 		{
 			value = ft_strchr(environ[i], '=') + 1;
 			break ;
