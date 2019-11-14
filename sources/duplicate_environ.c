387f185bbf67b0ced3f0277498f3895843613cc2
4slan <amalsago@student.42.fr>
2019-11-14 15:49:37 +0100

    change to more explicit function name

--- sources/duplicate_environ.c
+++ sources/duplicate_environ.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/12 18:36:39 by amalsago          #+#    #+#             */
-/*   Updated: 2019/11/13 15:48:43 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/11/14 14:30:01 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 extern char		**environ;
 
-int				total_rows()
+int				total_environ_rows(void)
 {
 	int			i;
 
@@ -30,7 +30,7 @@ char			**duplicate_environ(void)
 	char		**new_environ;
 
 	i = -1;
-	if (!(new_environ = ft_strnew2d(total_rows() + 1)))
+	if (!(new_environ = ft_strnew2d(total_environ_rows() + 1)))
 		return (NULL);
 	while (environ[++i])
 		new_environ[i] = ft_strdup(environ[i]);
