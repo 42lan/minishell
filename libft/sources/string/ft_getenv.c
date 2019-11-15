1f3f6ef7183d49b41381ac32147c762fdf118c9c
4slan <amalsago@student.42.fr>
2019-11-15 19:55:20 +0100

    pre-incrementing to gain one line

--- libft/sources/string/ft_getenv.c
+++ libft/sources/string/ft_getenv.c
@@ -6,11 +6,12 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/26 19:22:45 by amalsago          #+#    #+#             */
-/*   Updated: 2019/11/14 11:57:52 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/11/15 19:46:12 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include "ft_printf.h"
 
 extern char		**environ;
 
@@ -20,9 +21,9 @@ char			*ft_getenv(const char *name)
 	int			len;
 	char		*value;
 
-	i = 0;
+	i = -1;
 	value = NULL;
-	while (environ[i])
+	while (environ[++i])
 	{
 		len = ft_strlen(name);
 		if (ft_strnequ(environ[i], name, len) && environ[i][len] == '=')
@@ -30,7 +31,6 @@ char			*ft_getenv(const char *name)
 			value = ft_strchr(environ[i], '=') + 1;
 			break ;
 		}
-		i++;
 	}
 	return (value);
 }
