b0e3b94083ba6be6218c4155e0c30c1b67dea45d
4slan <amalsago@student.42.fr>
2019-11-19 21:15:43 +0100

    Increment deep level of minishell

--- sources/execution/execute_command.c
+++ sources/execution/execute_command.c
@@ -12,11 +12,19 @@
 
 #include "minishell.h"
 
-extern char		**environ;
+extern char			**environ;
 
-int				execute_command(char **command)
+void				increment_level(void)
 {
-	char		*realpath;
+	static int		lvl;
+
+	lvl = ft_atoi(ft_getenv("SHLVL"));
+	ft_setenv("SHLVL", ft_itoa(lvl + 1), 1);
+}
+
+int					execute_command(char **command)
+{
+	char			*realpath;
 
 	if (!(realpath = find_executable(command[0])))
 	{
@@ -25,8 +33,12 @@ int				execute_command(char **command)
 		exit(EXIT_FAILURE);
 	}
 	if (check_access(realpath))
+	{
+		if (ft_strequ(realpath, "./minishell"))
+			increment_level();
 		if ((execve(realpath, command, environ)) < 0)
 			ft_perror("minishell: execve() failed in child_handler()");
+	}
 	ft_strdel(&realpath);
 	exit(EXIT_FAILURE);
 }
