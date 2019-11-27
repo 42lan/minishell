745ce3875e2f4d7112a3312ba2821dee1b3d1d75
4slan <amalsago@student.42.fr>
2019-11-27 16:12:20 +0100

    defining OPEN_MAX if isn't on system

--- libft/includes/get_next_line.h
+++ libft/includes/get_next_line.h
@@ -6,21 +6,22 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/25 09:24:03 by amalsago          #+#    #+#             */
-/*   Updated: 2019/10/16 12:15:04 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/11/27 15:55:09 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# ifndef BUFF_SIZE_GNL
-#  define BUFF_SIZE_GNL 32
+# define BUFF_SIZE_GNL 32
+# ifndef OPEN_MAX
+#  define OPEN_MAX 2048
+# endif
 
-#  include <unistd.h>
-#  include <limits.h>
-#  include <string.h>
-#  include "libft.h"
+# include <unistd.h>
+# include <limits.h>
+# include <string.h>
+# include "libft.h"
 
 int		get_next_line(const int fd, char **line);
 
-# endif
 #endif
