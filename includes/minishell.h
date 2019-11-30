b8280669191f615d58e130ffb236c47d023dd42f
4slan <amalsago@student.42.fr>
2019-11-30 21:12:33 +0100

    adding argument to execute_command()

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/05 12:44:42 by amalsago          #+#    #+#             */
-/*   Updated: 2019/11/26 15:36:59 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/11/30 20:20:33 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,38 +18,38 @@
 # include "../libft/includes/ft_printf.h"
 # include "../libft/includes/get_next_line.h"
 
-typedef struct		s_msh
+typedef struct	s_msh
 {
-	char			*line;
-	char			*history_file;
-	char			**argv;
-	char			**environ;
-	char			**commands;
-}					t_msh;
+	char		*line;
+	char		*history_file;
+	char		**argv;
+	char		**environ;
+	char		**commands;
+}				t_msh;
 
-char	*get_input(void);
-char	**parse_input(const char *line);
-void	write_history(t_msh *data, const char *line);
+char			*get_input(void);
+char			**parse_input(const char *line);
+void			write_history(t_msh *data, const char *line);
 
-void	expand_symbols(char **command);
-char	*find_executable(const char *executable);
-int		check_access(const char *realpath);
-int		is_builtin(const char *command);
-int		execute(t_msh *data, const char *line);
-void	execute_builtin(t_msh *data, const char *command, const char *line);
-int		execute_command(char **command);
+void			expand_symbols(char **command);
+char			*find_executable(const char *executable);
+int				check_access(const char *realpath);
+int				is_builtin(const char *command);
+int				execute(t_msh *data, const char *line);
+void			execute_builtin(t_msh *data, const char *command, const char *line);
+int				execute_command(const char *realpath, char **command);
 
-void	print_eacces(const char *filename);
-void	print_enoent(const char *filename);
-void	print_enofound(const char *filename);
+void			print_eacces(const char *filename);
+void			print_enoent(const char *filename);
+void			print_enofound(const char *filename);
 
-void	display_prompt(void);
-void	clear_console(void);
-void	print_commands(char **commands);
-void	ft_arraydel(char **array);
+void			display_prompt(void);
+void			clear_console(void);
+void			print_commands(char **commands);
+void			ft_arraydel(char **array);
 
-char	**setup_environ(t_msh *data);
-int		total_environ_rows(void);
+char			**setup_environ(t_msh *data);
+int				total_environ_rows(void);
 
 int				builtin_builtins(const char *args);
 int				builtin_cd(const char *line);
