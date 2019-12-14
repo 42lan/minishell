8993daaf96a6bb7b5f2a634ead287f4e83ee992d
4slan <amalsago@student.42.fr>
2019-12-14 16:33:47 +0100

    incrementing level in main; receiving non null argument realpath

--- sources/execution/execute_command.c
+++ sources/execution/execute_command.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/27 11:33:04 by amalsago          #+#    #+#             */
-/*   Updated: 2019/12/01 16:02:21 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/12/02 17:53:49 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,9 @@
 
 extern char			**environ;
 
-void				increment_level(void)
-{
-	static int		lvl;
-
-	lvl = ft_atoi(ft_getenv("SHLVL"));
-	ft_setenv("SHLVL", ft_itoa(lvl + 1), 1);
-}
-
 int					execute_command(const char *realpath, char **command)
 {
-	if (check_access(realpath))
-	{
-		if (ft_strequ(realpath, "./minishell"))
-			increment_level();
-		if ((execve(realpath, command, environ)) < 0)
-			ft_perror("minishell: execve() failed in child_handler()");
-	}
+	if ((execve(realpath, command, environ)) < 0)
+		ft_perror("execve() failed in child_handler()");
 	exit(EXIT_FAILURE);
 }
