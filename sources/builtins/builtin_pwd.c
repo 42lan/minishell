674a4186e133f4c2f19695ebac8a7832b5639a09
4slan <amalsago@student.42.fr>
2019-12-17 23:53:27 +0100

    printing error message as csh

--- sources/builtins/builtin_pwd.c
+++ sources/builtins/builtin_pwd.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/20 11:39:59 by amalsago          #+#    #+#             */
-/*   Updated: 2019/11/28 17:42:29 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/12/17 23:51:46 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int			builtin_pwd(void)
 	if (cwd != NULL)
 		ft_printf("%s\n", cwd);
 	else
-		ft_perror("minishell: getcwd() returned NULL in builtin_pwd()");
+		ft_perror("pwd: .: Permission denied");
 	ft_strdel(&cwd);
 	return (0);
 }
