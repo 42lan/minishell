65a8d051d2da506931781cc91710541552465b42
4slan <amalsago@student.42.fr>
2019-12-24 03:50:39 +0100

    Open history file in cwd if HOME isn't setted

--- sources/tools/history.c
+++ sources/tools/history.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/20 16:06:56 by amalsago          #+#    #+#             */
-/*   Updated: 2019/11/26 15:27:44 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/12/23 05:23:08 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,19 @@
 
 void				write_history(t_msh *data, const char *line)
 {
-	int				fd;
-	int				mode;
+	char			*home_dir;
+	char			*filename;
 	int				oflag;
+	int				mode;
+	int				fd;
 
-	mode = S_IRUSR | S_IWUSR;
+	home_dir = NULL;
+	filename = ".minishell_history";
 	oflag = O_RDWR | O_APPEND | O_CREAT;
+	mode = S_IRUSR | S_IWUSR;
 	if (!data->history_file)
-		data->history_file = form_path(ft_getenv("HOME"), ".minishell_history");
+		data->history_file = ((home_dir = ft_getenv("HOME")))
+							? form_path(home_dir, filename) : filename;
 	if ((fd = open(data->history_file, oflag, mode)) < 0)
 		ft_perror("minishell: open() failed in write_history()");
 	else
