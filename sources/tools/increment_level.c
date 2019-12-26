7582911e24c887f98d42314f0fa1ea72543a9ce7
4slan <amalsago@student.42.fr>
2019-12-26 04:53:35 +0100

    incrementing SHLVL in proper way

--- sources/tools/increment_level.c
+++ sources/tools/increment_level.c
@@ -6,18 +6,18 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/02 15:45:10 by amalsago          #+#    #+#             */
-/*   Updated: 2019/12/03 12:55:56 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/12/26 04:52:57 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void				increment_level(void)
+void		increment_level(void)
 {
-	static int		level = 0;
+	int		level;
 
-	level++;
-	ft_printf(">>%d\n", level);
-	ft_setenv("SHLVL", ft_itoa_static(level), 1);
-	//ft_setenv("SHLVL", ft_itoa(level), 1); // MALLOC - NEED TO BE FREED
+	if (ft_getenv("SHLVL") == NULL)
+		ft_setenv("SHLVL", "0", OVERWRITE);
+	level = ft_atoi(ft_getenv("SHLVL")) + 1;
+	ft_setenv("SHLVL", ft_itoa_static(level), OVERWRITE);
 }
