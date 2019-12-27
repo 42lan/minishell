4497cd7ca29f5c87ef4ec03199625ec2af6dabb5
4slan <amalsago@student.42.fr>
2019-12-27 01:29:00 +0100

    check_access() returns non zero value if it fails

--- sources/builtins/builtin_cd.c
+++ sources/builtins/builtin_cd.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 19:08:58 by amalsago          #+#    #+#             */
-/*   Updated: 2019/12/26 04:56:08 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/12/26 23:08:34 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,7 @@ int					builtin_cd(const char *line)
 	cwd = NULL;
 	cwd = getcwd(cwd, 0);
 	if (!(path = determine_path(line, cwd))
-		|| !check_access(path) || chdir(path) < 0)
+		|| check_access(path) != 0 || chdir(path) < 0)
 	{
 		ft_strdel(&cwd);
 		return (0);
