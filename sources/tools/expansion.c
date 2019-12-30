39840de3ebc69bd1f54bd3e6ef19d57c3349436f
4slan <amalsago@student.42.fr>
2019-12-30 05:38:07 +0100

    lightenig expand_dollar_helper() and freeing according return value in expand_dollar()

--- sources/tools/expansion.c
+++ sources/tools/expansion.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/07 16:24:53 by amalsago          #+#    #+#             */
-/*   Updated: 2019/12/24 03:31:57 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/12/30 05:35:45 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,31 +31,31 @@ static void		expand_tilde(char **command)
 	ft_strdel(&tmp);
 }
 
-static void		expand_dollar_helper(char **variables, char **newstr)
+static int		expand_dollar_helper(char **variables, char **newstr)
 {
-	int			i;
 	char		*value;
 	char		*tmp_str;
 	char		*tmp_var;
 
-	i = -1;
-	while (variables[++i])
+	while (*variables)
 	{
-		tmp_var = variables[i];
-		variables[i] = ft_strtrim(variables[i]); // MALLOC
-		if ((value = ft_getenv(variables[i])))
+		tmp_var = *variables;
+		*variables = ft_strtrim(*variables); // MALLOC
+		ft_strdel(&tmp_var);
+		if ((value = ft_getenv(*variables)))
 		{
 			tmp_str = *newstr;
-			*newstr = ft_strjoin(*newstr, " "); // MALLOC
-			ft_strdel(&tmp_str);
-			tmp_str = *newstr;
-			*newstr = ft_strjoin(*newstr, value); // MALLOC
+			*newstr = ft_strjoin_sep(*newstr, value, " "); // MALLOC
 			ft_strdel(&tmp_str);
 		}
 		else
-			ft_printf("%s: Undefined variable\n", variables[i]);
-		ft_strdel(&tmp_var);
+		{
+			ft_printf("%s: Undefined variable\n", *variables);
+			return (0);
+		}
+		variables++;
 	}
+	return (1);
 }
 
 static void		expand_dollar(char **command)
@@ -64,11 +64,15 @@ static void		expand_dollar(char **command)
 	char		**variables;
 
 	variables = ft_strsplit(*command + ft_strcspn(*command, "$"), '$'); // MALLOC
-	newstr = ft_strsub(*command, 0, ft_strcspn(*command, "$"));
-	expand_dollar_helper(variables, &newstr);
+	newstr = ft_strsub(*command, 0, ft_strcspn(*command, "$")); // MALLOC
+	if ((expand_dollar_helper(variables, &newstr)) == 1)
+	{
+		ft_strdel(command);
+		*command = newstr;
+	}
+	else
+		ft_strdel(&newstr);
 	ft_strarraydel(&variables);
-	ft_strdel(command);
-	*command = newstr;
 }
 
 void			expand_symbols(char **command)
