5a42019a2332421725582406d092d9cf191f7593
4slan <amalsago@student.42.fr>
2020-01-03 11:01:56 +0100

    builtin_setenv() no longer receives data because changing data->environ cause a leak founded by @jates-

--- sources/builtins/builtin_setenv.c
+++ sources/builtins/builtin_setenv.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 18:32:36 by amalsago          #+#    #+#             */
-/*   Updated: 2019/12/31 03:38:44 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/01/03 10:51:16 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,28 +21,26 @@
 **		Upon successful completion, the value 1 is returned otherwise 0.
 */
 
-int					builtin_setenv(const char *line, t_msh *data)
+int					builtin_setenv(const char *line)
 {
 	int				ret;
 	char			*name;
 	char			*value;
-	char			*trimmed;
-	extern char		**environ;
+	char			*variable;
 
 	ret = 1;
-	trimmed = ft_strtrim(line);
-	if (!*trimmed)
+	variable = ft_strtrim(line);
+	if (!*variable)
 		builtin_env();
 	else
 	{
-		name = ft_strtok(trimmed, " \t");
-		value = (trimmed[ft_strlen(name) + 1] == '"')
-				? ft_strctrim(trimmed + ft_strlen(name) + 1, '"')
+		name = ft_strtok(variable, " \t");
+		value = (variable[ft_strlen(name) + 1] == '"')
+				? ft_strctrim(variable + ft_strlen(name) + 1, '"')
 				: ft_strctrim(ft_strtok(NULL, " \t"), ' ');
 		ret = ft_setenv(name, value, OVERWRITE);
-		data->environ = environ;
 		ft_strdel(&value);
 	}
-	ft_strdel(&trimmed);
+	ft_strdel(&variable);
 	return (ret);
 }
