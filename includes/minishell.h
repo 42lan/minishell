28b8347d2de2a56a63158ac35ca86338301a2563
4slan <amalsago@student.42.fr>
2020-01-06 14:43:09 +0100

    including colors.h

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,13 +6,14 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/05 12:44:42 by amalsago          #+#    #+#             */
-/*   Updated: 2020/01/06 09:53:52 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/01/06 14:26:41 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include "colors.h"
 # include "system_libraries.h"
 # include "../libft/includes/libft.h"
 # include "../libft/includes/ft_printf.h"
