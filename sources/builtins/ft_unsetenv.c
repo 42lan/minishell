e1c60faf16fdf0dab60a8c42ea15f14352ef19c1
4slan <amalsago@student.42.fr>
2020-01-06 08:50:53 +0100

    trim will be made in builtin_unsetenv()

--- sources/builtins/ft_unsetenv.c
+++ sources/builtins/ft_unsetenv.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/10 12:07:18 by amalsago          #+#    #+#             */
-/*   Updated: 2019/12/31 03:41:18 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/01/01 08:12:01 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,27 +39,23 @@ int					ft_unsetenv(const char *name)
 {
 	int				i;
 	int				len;
-	char			*trimmed_name;
 	extern char		**environ;
 
 	i = -1;
-	trimmed_name = ft_strtrim(name);
-	if (!trimmed_name || !*trimmed_name)
+	if (!name || !*name)
 	{
 		ft_perror("unsetenv: Too few arguments.");
-		ft_strdel(&trimmed_name);
 		return (-1);
 	}
 	while (environ[++i])
 	{
-		len = ft_strlen(trimmed_name);
-		if (ft_strnequ(environ[i], trimmed_name, len) && environ[i][len] == '=')
+		len = ft_strlen(name);
+		if (ft_strnequ(environ[i], name, len) && environ[i][len] == '=')
 		{
 			ft_strdel(&environ[i]);
 			shift_environ(i);
 			break ;
 		}
 	}
-	ft_strdel(&trimmed_name);
 	return (0);
 }
