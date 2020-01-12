75cc616d5111e5616ef0724b0542fba0b05e8951
4slan <amalsago@student.42.fr>
2020-01-12 03:28:55 +0100

    check if variable is NULL write error message

--- sources/builtins/builtin_unsetenv.c
+++ sources/builtins/builtin_unsetenv.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/06 09:42:18 by amalsago          #+#    #+#             */
-/*   Updated: 2020/01/06 09:49:16 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/01/12 03:23:20 by aslan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,10 @@ void		builtin_unsetenv(const char *line)
 
 	i = -1;
 	variables = ft_strsplit_spaces(line);
-	while (variables[++i])
-		ft_unsetenv(variables[i]);
+	if (variables)
+		while (variables[++i])
+			ft_unsetenv(variables[i]);
+	else
+		ft_perror(E_FEWARGS);
 	ft_strarraydel(&variables);
 }
